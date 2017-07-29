bb73187ef994e82bf57b5f8fc8a2fccfe89222a1
Cassie Wang <352211966@qq.com>
2017-07-30 04:32:17 +1200

    Initial commit

---
+++ try2/AzureManerger.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try2
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<NotHotDogModel> notHotDogTable;
+
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://module2try.azurewebsites.net");
+            this.notHotDogTable = this.client.GetTable<NotHotDogModel>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+        public async Task<List<NotHotDogModel>> GetHotDogInformation()
+        {
+            return await this.notHotDogTable.ToListAsync();
+        }
+    }
+}
---
+++ try2/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace try2.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
